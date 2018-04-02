fffaa7d107c02871c45da1b6c39fe39b4a371c01
David Bonnie <dbonnie@lanl.gov>
2018-04-02 15:20:13 -0600

    Updated linked list to have a tail pointer as we changed live in prod.  Reduces time to queue new work to constant time.

--- src/pftool.h
+++ src/pftool.h
@@ -24,7 +24,7 @@
 int manager(int rank, struct options& o, int nproc, path_list *input_queue_head, path_list *input_queue_tail, int input_queue_count, const char *dest_path);
 void manager_workdone(int rank, int sending_rank, struct worker_proc_status *proc_status, int* readdir_rank_count);
 int  manager_add_paths(int rank, int sending_rank, path_list **queue_head, path_list **queue_tail, int *queue_count);
-void manager_add_buffs(int rank, int sending_rank, work_buf_list **workbuflist, int *workbufsize);
+void manager_add_buffs(int rank, int sending_rank, work_buf_list **workbuflist, work_buf_list **workbuftail, int *workbufsize);
 void manager_add_copy_stats(int rank, int sending_rank, int *num_copied_files, size_t *num_copied_bytes);
 void manager_add_examined_stats(int rank, int sending_rank, int *num_examined_files, size_t *num_examined_bytes, int *num_examined_dirs, size_t *num_finished_bytes);
 void send_manager_examined_stats(int num_examined_files, size_t num_examined_bytes, int num_examined_dirs);
